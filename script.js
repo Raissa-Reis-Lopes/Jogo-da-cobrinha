2f029699c740593774219b934be0bcad521dfbeb
Raissa <raissa.vilemon@gmail.com>
2022-05-30 07:05:13 -0300

    the snake

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ let context = canvas.getContext("2d"); //....
 let box = 32;
 let snake = []; //criar cobrinha como lista, já que ela vai ser uma série de coordenadas, que quando pintadas, criam os quadradinhos
 snake[0] ={
-    x: 8 * box,
-    y: 8 * box
+    x: 4 * box,
+    y: 4 * box
 }
 let direction = "right";
 let food ={
